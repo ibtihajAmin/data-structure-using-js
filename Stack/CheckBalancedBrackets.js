b048b2a3dbba820c702adec654abb96007bb0dda
ibtihajAmin <ibtihaj.amin18@gmail.com>
2023-05-01 21:40:16 +0600

    Check Balanced Brackets Solution is added

--- Stack/CheckBalancedBrackets.js
+++ Stack/CheckBalancedBrackets.js
@@ -1,6 +1,39 @@
-/*Check for Balanced Brackets in an expression using Stack */
-
-// Doing check if the brackets are balanced
-const checkBalancedBrackets = (str) => {
+function checkBalancedBrackets(str) {
   let stack = [];
-};
+
+  for (let i = 0; i < str.length; i++) {
+    let temp = str[i];
+
+    if (temp == "(" || temp == "[" || temp == "{") {
+      stack.push(temp);
+      continue;
+    }
+
+    if (stack.length == 0) return false;
+
+    let check;
+    switch (temp) {
+      case ")":
+        check = stack.pop();
+        if (check == "{" || check == "[") return false;
+        break;
+
+      case "}":
+        check = stack.pop();
+        if (check == "(" || check == "[") return false;
+        break;
+
+      case "]":
+        check = stack.pop();
+        if (check == "(" || check == "{") return false;
+        break;
+    }
+  }
+
+  return stack.length == 0;
+}
+
+let str = "[{()}]";
+
+if (checkBalancedBrackets(str)) console.log("Balanced ");
+else console.log("Not Balanced ");
